6be69d8951e2e38be732907388d514818f06b543
Kidus Adugna <kidusadugna@gmail.com>
2019-09-15 17:30:55 +0300

    add data merging and connection problem handling

--- mezmur/teleg.py
+++ mezmur/teleg.py
@@ -10,10 +10,12 @@ from os import chdir, path, remove
 from sys import argv
 
 from telethon.sync import TelegramClient
-from telethon.tl.types import PeerChat
-from telethon.tl.types import InputMessagesFilterEmpty
+from telethon.tl.types import PeerChat, InputMessagesFilterEmpty
 from telethon.tl.functions.messages import SearchRequest
-# from telethon.errors.rpc_errors_401 import SessionPasswordNeededError
+
+MEZ_BEGIN = 'መዝ('  # the beginning of the mez
+# the data collection file
+DATA_FILE = 'mez-data.json'
 
 # command line arguments
 if len(argv) == 1:
@@ -39,18 +41,22 @@ phone = '+251920810739'
 username = 'K1DV5'
 
 client = TelegramClient(username, api_id, api_hash)
-client.connect()
-
+try:
+    client.connect()
     # Ensure you're authorized
-if not client.is_user_authorized():
-    client.send_code_request(phone)
-    try:
-        client.sign_in(phone, input('Enter the code: '))
-    except:
-        client.sign_in(password=input('Password: '))
+    if not client.is_user_authorized():
+        client.send_code_request(phone)
+        try:
+            client.sign_in(phone, input('Enter the code: '))
+        except:
+            client.sign_in(password=input('Password: '))
+    # chat = client.get_entity(PeerChat(269484381)) # The main group
+    chat = client.get_entity('MezCollectorBot')
+    CONNECTED = True
 
-# chat = client.get_entity(PeerChat(269484381))
-chat = client.get_entity('MezCollectorBot')
+except ConnectionError:
+    print('Connection problem.')
+    CONNECTED = False
 
 today = convert_date(date.today())
 
@@ -64,113 +70,123 @@ if command == 'post':
     copy('../dist/index.html', main_fname)
     # build the basic version
     basic_fname = build_basic()
+    print('Built documents')
 
-    client.send_file(chat, main_fname, caption="Normal version")
-    client.send_file(chat, basic_fname, caption="Basic version")
-    print('Uploaded data.')
+    if CONNECTED:
+        client.send_file(chat, main_fname, caption="Normal version")
+        client.send_file(chat, basic_fname, caption="Basic version")
+        print('Uploaded data.')
     exit()
+
 
 # get data
 
-mez_begin = 'መዝ('
-def extract_title(mez: str):
-    mez = mez.strip()
-    in_parens_start = len(mez_begin)
-    in_parens_end = mez.find(')', in_parens_start)
-    in_parens = mez[in_parens_start: in_parens_end]
-    if '/' in in_parens:
-        category, title = in_parens.split('/')
-    else:
-        category, title = 'የምስጋና', in_parens.strip()
-    body = mez[in_parens_end + 1:].strip()
-    return {
-            'title': title,
-            'body': body,
-            'category': category,
-            }
+if path.exists(DATA_FILE):
+    # get the data in the file
+    with open(DATA_FILE, encoding='utf-8') as file:
+        collected = load(file)
+        offset_id = collected['last_id']
+else:
+    collected = {'data': {}, 'count_eng': 0}
+    offset_id = 0
 
+if CONNECTED:
+    # option 1: get all messages and filter them here
+    # messages = client.iter_messages(fk_entity)
+    # messages = client.get_messages('MezCollectorBot')
 
-# option 1: get all messages and filter them here
-# messages = client.iter_messages(fk_entity)
-# messages = client.get_messages('MezCollectorBot')
+    # option 2: get filtered messages from there
+    filter = InputMessagesFilterEmpty()
+    result = client(SearchRequest(
+        peer=chat,  # On which chat/conversation
+        q=MEZ_BEGIN,  # What to search for
+        filter=filter,  # Filter to use (maybe filter for media)
+        min_date=None,  # Minimum date
+        max_date=None,  # Maximum date
+        offset_id=offset_id,  # ID of the message to use as offset
+        add_offset=0,  # Additional offset
+        limit=5,  # How many results
+        max_id=0,  # Maximum message ID
+        min_id=0,  # Minimum message ID
+        from_id=None,  # Who must have sent the message (peer)
+        hash=0  # Special number to return nothing on no-change
+        ))
+    messages = result.messages
 
-# option 2: get filtered messages from there
-filter = InputMessagesFilterEmpty()
-result = client(SearchRequest(
-    peer=chat,  # On which chat/conversation
-    q=mez_begin,  # What to search for
-    filter=filter,  # Filter to use (maybe filter for media)
-    min_date=None,  # Minimum date
-    max_date=None,  # Maximum date
-    offset_id=0,  # ID of the message to use as offset
-    add_offset=0,  # Additional offset
-    limit=5,  # How many results
-    max_id=0,  # Maximum message ID
-    min_id=0,  # Minimum message ID
-    from_id=None,  # Who must have sent the message (peer)
-    hash=0  # Special number to return nothing on no-change
-))
-messages = result.messages
-users = result.users
-
-collected = {
-        'date': today,
-        'data': {},
-        'last_id': messages[0].id,
-        'count_eng': 0
-        }
-
-for message in messages:
-    message_cont = message.message
-    if message_cont and message_cont.strip().startswith(mez_begin):
-        collected['count_eng'] += 1
-        message_id = message.id
-        sender = [user for user in users if message.from_id == user.id][0]
-        sender_username = sender.username
-        sender_link = f'https://t.me/{sender_username}'
-        sender_fname = sender.first_name
-        sender_lname = sender.last_name
-        if sender_fname and sender_lname:
-            sender_name = f'{sender_fname} {sender_lname}'
-        elif sender_fname:
-            sender_name = sender_fname
-        elif sender_lname:
-            sender_name = sender_lname
+    def extract_title(mez: str):
+        mez = mez.strip()
+        in_parens_start = mez.find(MEZ_BEGIN) + len(MEZ_BEGIN)
+        in_parens_end = mez.find(')', in_parens_start)
+        in_parens = mez[in_parens_start: in_parens_end]
+        if '/' in in_parens:
+            category, title = in_parens.split('/')
         else:
-            sender_name = sender_username
-
-        mez_data = extract_title(message_cont)
-        category = mez_data['category']
-        mez = {
-            'sender_name': sender_name,
-            'sender_link': sender_link,
-            'title': mez_data['title'],
-            'body': mez_data['body'],
-            'id': message_id,
-            'date': convert_date(message.date.date()),
+            category, title = 'የምስጋና', in_parens.strip()
+        body = mez[in_parens_end + 1:].strip()
+        return {
+                'title': title,
+                'body': body,
+                'category': category,
                 }
-        if category in collected['data']:
-            collected['data'][category]['data'].append(mez)
-            collected['data'][category]['count_eng'] += 1
-            collected['data'][category]['count'] = geez_num(collected['data'][category]['count_eng'])
-        else:
-            collected['data'][category] = {
-                    'count': geez_num(1),
-                    'count_eng': 1,
-                    'data': [mez]
-                    }
 
-# count data
-collected['count'] = geez_num(collected['count_eng'])
+    if messages: # there are new messages
+        users = result.users
 
-file_name = 'mez.json'
+        collected['date'] = today
+        collected['last_id'] = messages[0].id
 
-if path.exists(file_name):
-    # get the date in the file
-    with open(file_name, encoding='utf-8') as file:
-        old_date = load(file)['date']
-    move(file_name, 'archive/' + 'mez-' + old_date.replace(' ', '-') + '.json')
+        for message in messages:
+            message_cont = message.message
+            if message_cont and message_cont.strip().startswith(MEZ_BEGIN):
+                collected['count_eng'] += 1
+                message_id = message.id
+                sender = [user for user in users if message.from_id == user.id][0]
+                sender_username = sender.username
+                sender_link = f'https://t.me/{sender_username}'
+                sender_fname = sender.first_name
+                sender_lname = sender.last_name
+                if sender_fname and sender_lname:
+                    sender_name = f'{sender_fname} {sender_lname}'
+                elif sender_fname:
+                    sender_name = sender_fname
+                elif sender_lname:
+                    sender_name = sender_lname
+                else:
+                    sender_name = sender_username
 
-with open(file_name, 'w', encoding='utf-8') as file:
+                mez_data = extract_title(message_cont)
+                category = mez_data['category']
+                mez_title = mez_data['title']
+                mez_attrib = {
+                        'sender_name': sender_name,
+                        'sender_link': sender_link,
+                        'body': mez_data['body'],
+                        'id': message_id,
+                        'date': convert_date(message.date.date()),
+                        }
+                # increment the total count
+                collected['count_eng'] += 1
+                if category in collected['data']:
+                    collected['data'][category]['data'][mez_title] = mez_attrib
+                    # increment the category count
+                    collected['data'][category]['count_eng'] += 1
+                    collected['data'][category]['count'] = geez_num(collected['data'][category]['count_eng'])
+                else:
+                    collected['data'][category] = {
+                            'count': geez_num(1),
+                            'count_eng': 1,
+                            'data': {mez_title: mez_attrib}
+                            }
+
+                    # count data
+        collected['count'] = geez_num(collected['count_eng'])
+
+
+    else:
+        print('There are no new messages')
+
+with open(DATA_FILE, 'w', encoding='utf-8') as file:
     dump(collected, file, ensure_ascii=False)
-print(f'Exported data to "{file_name}"')
+
+print(f'Updated data in "{DATA_FILE}"')
+
