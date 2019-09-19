5e147561d6fccbac521fa9d8c92f8874fe537853
Kidus Adugna <kidusadugna@gmail.com>
2019-09-19 17:15:02 +0300

    viewer: handle all repeats nicely

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -1,45 +1,81 @@
 import React from 'react'
 
-export default (props) => {
-    let body = props.body
-    // take care of repeats
-    let parts = []
-    let inParens = false
-    let key = 0
-    for (let part of body.split(/\n\(([^]*)\)/)) {
-        let partLines = part.split('\n')
-        if (inParens) {
-            key++
-            parts.push(
-                <div
-                    key={key}
-                    style={{
-                        display: 'inline-flex',
-                        alignItems: 'center',
-                    }}>
-                    <div>
-                        {partLines.map((line, i) =>
-                            <p style={{margin: 0}} key={i}>{line}</p>)}
-                    </div>
-                    <div style={{fontSize: partLines.length + 'em', marginBottom: '.15em'}}>{'}'}</div>
-                </div>
-            )
-            inParens = false
-        } else {
-            for (let line of partLines) {
-                key++
-                parts.push(
-                    <React.Fragment key={key}>{line}<br /></React.Fragment>
-                )
-            }
-            inParens = true
-        }
+function Line(props) {
+    if (/\[.*?\]/.test(props.line)) {
+        return (
+            <React.Fragment>{props.line.split(/\[([^]*?\] *[\u1369-\u1371])/).map((part, i) => {
+                if (i % 2) { // in parens
+                    let [text, number] = part.split(']')
+                    return <React.Fragment key={i}>
+                                <u>{text}</u> {number}
+                            </React.Fragment>
+                }
+                return <React.Fragment key={i}>{part}</React.Fragment>
+            })}
+            </React.Fragment>
+        )
     }
+    return props.line.trim()? props.line : null
+}
+
+function Verse(props) {
+    // like a paragraph, without any blank line
+    let text = props.children.replace('(', '[').replace(')', ']')
+
+    return (
+        <div style={{marginBottom: '1em'}}>
+            {text.split(/\(([^]*?\) *[\u1369-\u1371])/).map((part, i) => { // take care of multiline repeats
+                let [text, number] = part.split(')')
+                let partLines = text.trim().split('\n')
+                if (i % 2) {  // in parens
+                    return (
+                        <div
+                            key={i}
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <div>
+                                {partLines.map((line, j) =>
+                                    <React.Fragment key={j}>
+                                        <Line line={line} />
+                                        <br />
+                                    </React.Fragment>)}
+                            </div>
+                            <div
+                                style={{
+                                    fontSize: partLines.length + 'em',
+                                    marginBottom: '.15em'
+                                }}>{'}'}</div> {number}
+                        </div>
+                    )
+                } else {
+                    return (
+                        <React.Fragment key={i}>
+                            {partLines.map((line, j) =>
+                                <React.Fragment key={j}>
+                                    <Line line={line} />
+                                    <br />
+                                </React.Fragment>
+                            )}
+                        </React.Fragment>
+                    )
+                }
+            })}
+        </div>
+    )
+}
+
+export default (props) => {
     return (
         <div>
             <h2>{props.title}</h2>
             <div style={{fontSize: '19pt'}}>
-                {parts.map((line, key) => line)}
+                {props.body.split('\n\n').map((verse, key) => {
+                    if (verse.trim())
+                        return <Verse key={key}>{verse.trim()}</Verse>
+                })}
             </div>
         </div>
     )
