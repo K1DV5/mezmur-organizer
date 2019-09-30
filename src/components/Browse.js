1761e229a0e963516375fbda147f82e6d61bc75c
Kidus Adugna <kidusadugna@gmail.com>
2019-09-30 14:20:45 +0300

    fix contact link mistake

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -7,11 +7,11 @@ function ListItem(props) {
     let contactText
     if (props.contact) {
         if (isNaN(props.contact)) {  // username
-            contactLink = '@' + props.contact
-            contactText = contactLink
-        } else { // phone number 2519...
             contactLink = 'https://t.me/' + props.contact
-            contactText = 'tel:+' + props.contact
+            contactText = '@' + props.contact
+        } else { // phone number 2519...
+            contactLink = 'tel:+' + props.contact
+            contactText = '+' + props.contact
         }
     }
     return (
